78f6bee344d630c8b03a43600fd9681a0b68eb52
Eugene <>
2023-04-10 18:33:12 +0200

    Выполнили задание 29

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 ﻿//Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B
 
-/*Console.Write("Введите число A: ");
+Console.Write("Введите число A: ");
 int a = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите число B: ");
 int b = Convert.ToInt32(Console.ReadLine());
@@ -16,11 +16,11 @@ b = b - 1;
 }
 Console.Write($" A в натуральной степени B равно {c}");
 }
-*/
+
 
 //Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
 
-/*Console.Write("Введите число: ");
+Console.Write("Введите число: ");
 int a = Convert.ToInt32(Console.ReadLine());
 int len = Length(a); 
 Console.Write($"Сумма в {a} равна: {Sum(a,len)}");
@@ -44,7 +44,13 @@ a /= 10;
 }
 return sum;
 }
-*/
+
 
 //Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
 
+int [] randomArray = new int [8];
+for (int i=0; i<8;i++) 
+{
+    randomArray[i] = new Random().Next(1,99);
+    Console.Write(randomArray[i] + " ");
+}
