e82056d6d69d722dcfd9d2efa14a971252d10041
Eugene <>
2023-04-10 18:22:17 +0200

    Выполнили задание 27

--- Program.cs
+++ Program.cs
@@ -19,3 +19,32 @@ Console.Write($" A в натуральной степени B равно {c}");
 */
 
 //Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+/*Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int len = Length(a); 
+Console.Write($"Сумма в {a} равна: {Sum(a,len)}");
+int Length(int a)
+{
+int index = 0;
+while (a > 0)
+{
+a /= 10;
+index++;
+}
+return index;
+}
+int Sum(int a, int len)
+{
+int sum = 0;
+for (int i = 1; i <= len; i++)
+{
+sum += a % 10;
+a /= 10;
+}
+return sum;
+}
+*/
+
+//Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
